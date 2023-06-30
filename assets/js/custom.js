2080112e670506aedbffa4a4c059b83dc921a469
JPysus <johnmichaeldb@gmail.com>
2023-06-30 11:38:23 +0800

    heavily changed js file
    most addition is about generating html content

---
+++ assets/js/custom.js
@@ -0,0 +1,124 @@
+
+/*
+card_cert list
+the variable 'data' is from <script src="assets/json/generated_filenames.json"></script>
+*/
+
+var htmldata = ""
+  for (var i = 0; i < cert_list.length; i++) {
+    htmldata += `
+              <div class="col">
+                <div class="card card_cert btn">
+                  <img value="`+i+`"src="./assets/images/certs/`+cert_list[i]['src']+`" class="rounded-start p-3" alt="`+cert_list[i]['alt']+`">
+                  <div class="card-body">
+                    <h5 class="card-title text-center">`+cert_list[i]['alt']+`</h5>
+                  </div>  
+                </div>
+              </div>
+
+    `
+  }
+$('#cert_container').append(htmldata)
+
+/*
+  carousel
+  left key, previous image
+  right key, next image
+*/
+$(document).bind('keyup', function(e) {
+    if(e.which == 39){
+        $('.carousel').carousel('next');
+    }
+    else if(e.which == 37){
+        $('.carousel').carousel('prev');
+    }
+});
+
+$(document).ready(function(){
+  /*
+  Navbar Scroll
+  */
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 0, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  /*
+  skill_cards
+  the variable 'data' is from <script src="assets/json/generated_filenames.json"></script>
+  */
+  var htmldata = ""
+  for (var i = 0; i < data.length; i++) {
+    htmldata += `
+              <div class="col">
+                <div class="skill_div p-3">
+                  <img class="skill_img d-block pb-3" src="assets/images/icons/`+data[i]+`"/>
+                  <h4 class="skill_name">`+data[i].substr(0, data[i].length - 4).replace('_', ' ')+`</h4>
+                </div>
+              </div>
+
+    `
+  }
+  $('#skill_cards').append(htmldata)
+
+  /*
+  card_cert on click, show modal
+  */
+  $('.card_cert').on('click', function(event){
+    var indicator = ""
+    var inner = ""
+    let index = $(this).find('img').attr('value')
+
+    indicator += `<button type="button" data-bs-target="#modal_car" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>\n\n`
+    inner += `
+      <div class="carousel-item active">
+        <img src="./assets/images/certs/`+cert_list[index]['src']+`" class="w-100" alt="`+cert_list[index]['alt']+`">
+        <div class="carousel-caption position-static pb-0 d-none d-md-block">
+          <h5>`+cert_list[index]['hed']+`</h5>
+          <p>`+cert_list[index]['add']+`</p>
+        </div>
+      </div>
+    `
+    $('.car_cont').hide()
+
+    if(cert_list[index].hasOwnProperty('mor') && Array.isArray(cert_list[index]['mor'])) {
+      for(var i=0; i < cert_list[index]['mor'].length; i++){
+
+        indicator += `<button type="button" data-bs-target="#modal_car" data-bs-slide-to="`+(i+1)+`"aria-label="Slide `+(i+2)+`"></button>\n\n`
+        inner += `
+      <div class="carousel-item">
+        <img src="./assets/images/certs/`+cert_list[index]['mor'][i]['src']+`" class="w-100" alt="`+cert_list[index]['mor'][i]['hed']+`">
+        <div class="carousel-caption position-static pb-0 d-none d-md-block">
+          <h5>`+cert_list[index]['mor'][i]['hed']+`</h5>
+          <p>`+cert_list[index]['mor'][i]['add']+`</p>
+        </div>
+      </div>
+        `
+
+      }
+      $('.car_cont').show()
+    }
+
+    $('.carousel-inner').html(inner)
+    $('.carousel-indicators').html(indicator)
+
+    $('#modal').modal('show')
+  })
+});
