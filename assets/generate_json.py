198e05fcb9fee194ad5ab79dbdc52e47948f2bc9
JPysus <johnmichaeldb@gmail.com>
2024-05-04 10:27:27 +0800

    dynamic local file path

--- assets/generate_json.py
+++ assets/generate_json.py
@@ -1,11 +1,11 @@
-from os import listdir
+from os import listdir, getcwd
 from os.path import isfile, join
-mypath = r'G:\Documents\GitHub\jpysus.github.io\assets\images\icons'
+mypath = getcwd() + "\\images\\icons"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 import json
 onlyfiles = json.dumps(onlyfiles, indent=4)
 
-jsonpath = r'G:\Documents\GitHub\jpysus.github.io\assets\json\generated_filenames.json'
+jsonpath = getcwd() +"\\json\\generated_filenames.json"
 with open(jsonpath, "w") as outfile:
     outfile.write('const data = '+str(onlyfiles))
