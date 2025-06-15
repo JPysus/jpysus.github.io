a99ed488fc82bb62aa67cbe297c9c7401f0434cc
JPysus <johnmichaeldb@gmail.com>
2025-06-15 14:45:04 +0800

    changed steam_btn used data for display

--- assets/js/steam_btn.js
+++ assets/js/steam_btn.js
@@ -1,33 +1,3 @@
-function ajaxCall_steamBtn(){
-  $.ajax({
-    type: 'GET',
-    dataType: 'json',
-    url: 'https://transpose.pythonanywhere.com/_steam_GetPlayerSummaries',
-    crossDomain: true,
-    xhrFields: {
-      withCredentials: false,
-    },
-  })
-  .done(function (data) {
-    
-    $('#steam_btn').attr('class', 'steam-status col-2')
-    if(data.gameid === undefined && data.personastate !== 0) 
-      $('#steam_btn').addClass('online')
-    else if(data.gameid !== undefined) 
-      $('#steam_btn').addClass('gaming')
-    else 
-      $('#steam_btn').addClass('offline')
-
-    if([3, 4, 5].includes(data.personastate))
-      $('#steam_btn').addClass('away')
-
-
-      $('#steam_btn img').attr('src', data.avatar)
-      $('#steam_btn p').text(data.personaname)
-  })
-  .fail(function (xhr, textStatus, errorThrown) {
-  });
-}
 function ajaxCall_steamMiniprofile(){
   $.ajax({
     type: 'GET',
@@ -40,17 +10,26 @@ function ajaxCall_steamMiniprofile(){
   })
   .done(function (data) {
     $("#steam_miniprofile").html(DOMPurify.sanitize(data))
+    console.log()
+    $('#steam_btn').attr('class', 'steam-status col-2')
+    if($('#steam_miniprofile span.friend_status_online').html()) 
+      $('#steam_btn').addClass('online')
+    else if($('#steam_miniprofile span.game_state').html()) 
+      $('#steam_btn').addClass('gaming')
+    else if($('#steam_miniprofile span.friend_status_offline').html())
+      $('#steam_btn').addClass('offline')
+
+      $('#steam_btn img').attr('src', $('#steam_miniprofile .playersection_avatar img').attr('src'))
+      $('#steam_btn p').text($('#steam_miniprofile span.persona').html())
   })
   .fail(function (xhr, textStatus, errorThrown) {
     $("#steam_miniprofile").html("")
   });
 }
 $(document).ready(function(){
-  ajaxCall_steamBtn()
   ajaxCall_steamMiniprofile()
   $("#steam_btn a.btn")
     .on("mouseenter",function(){$("#steam_miniprofile").fadeIn( "fast" );})
     .on("mouseleave",function(){$("#steam_miniprofile").fadeOut( "fast" );})
-  setInterval(function(){ ajaxCall_steamBtn(); ajaxCall_steamMiniprofile()}, 10000);
 
 })
